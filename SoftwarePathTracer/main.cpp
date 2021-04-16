a0d746de392fe955bf52893313ce65212d0cadd4
Dutchman97 <makatipu97@hotmail.com>
2021-04-16 16:48:28 +0200

    Add keyboard input and buffer clearing

--- SoftwarePathTracer/main.cpp
+++ SoftwarePathTracer/main.cpp
@@ -9,7 +9,7 @@ int windowHeight = 1080 / 2;
 /// <summary>
 /// Perform any logic required before terminating the program.
 /// </summary>
-void terminate() {
+inline void terminateProgram() {
 	glfwTerminate();
 }
 
@@ -25,8 +25,22 @@ void framebufferResizeCallback(GLFWwindow* window, int newWidth, int newHeight) 
 	glViewport(0, 0, newWidth, newHeight);
 }
 
+/// <summary>
+/// GLFW callback function for key input.
+/// </summary>
+/// <param name="window">GLFW window object.</param>
+/// <param name="key">The keyboard key that was pressed or released.</param>
+/// <param name="scancode">The system-specific scancode of the key.</param>
+/// <param name="action">GLFW_PRESS, GLFW_RELEASE, or GLFW_REPEAT.</param>
+/// <param name="mod">Bit field describing which modifier keys were held down.</param>
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mod) {
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
 int main() {
-	// Initialize the GLFW window
+	// Initialize the GLFW window.
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
@@ -35,28 +49,34 @@ int main() {
 	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Path Tracer", nullptr, nullptr);
 	if (window == nullptr) {
 		std::cout << "Unable to create GLFW window." << std::endl;
-		terminate();
+		terminateProgram();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
 
-	// Initialize glad and OpenGL
+	// Initialize glad and OpenGL.
 	int loadGlSuccess = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 	if (!loadGlSuccess) {
 		std::cout << "Unable to initialize glad." << std::endl;
-		terminate();
+		terminateProgram();
 		return -1;
 	}
 	glViewport(0, 0, windowWidth, windowHeight);
 	glfwSetFramebufferSizeCallback(window, framebufferResizeCallback);
+	glfwSetKeyCallback(window, keyCallback);
 	
 
-	// Main loop
+	// Main loop.
 	while (!glfwWindowShouldClose(window)) {
-		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+		// Set the color to clear with and clear the color buffer.
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
 	}
 
-	terminate();
+	terminateProgram();
 	return 0;
 }
